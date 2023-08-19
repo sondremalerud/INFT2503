2dec707aab133a76e02108ee60ed5f731eecbcf5
Sondre Malerud <sondrmal@stud.ntnu.no>
2023-08-19 15:04:16 +0200

    Ferdig med oppg 5

--- 02/task5.cpp
+++ 02/task5.cpp
@@ -1,3 +1,23 @@
-//
-// Created by Sondre on 19.08.2023.
-//
+#include <iostream>
+
+int main() {
+    double number;
+
+    // Peker som peker til number
+    double *pointer = &number;
+
+    // Referanse som refererer til number
+    double &reference = number;
+
+    // Tre måter å tilordne verdi til number. Utskrift skal se slik ut: 123
+    number = 1;
+    std::cout << number;
+
+    *pointer = 2;
+    std::cout << number;
+
+    reference = 3;
+    std::cout << number;
+
+    return 0;
+}
